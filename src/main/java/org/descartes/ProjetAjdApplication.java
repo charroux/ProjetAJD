44f2618c7142aff92423e8af2a162728df71f7a6
charroux <benoit.charroux@free.fr>
2016-12-15 09:38:33 +0100

    first commit

---
+++ src/main/java/org/descartes/ProjetAjdApplication.java
@@ -0,0 +1,12 @@
+package org.descartes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetAjdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetAjdApplication.class, args);
+	}
+}
