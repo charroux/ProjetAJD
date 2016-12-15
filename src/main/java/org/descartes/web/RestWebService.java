761790e92f9306265afbc4722b950679024e0418
charroux <benoit.charroux@free.fr>
2016-12-15 10:16:31 +0100

    JPA

--- src/main/java/org/descartes/web/RestWebService.java
+++ src/main/java/org/descartes/web/RestWebService.java
@@ -1,8 +1,14 @@
 package org.descartes.web;
 
+import java.util.List;
+
+import location.Car;
+
 import org.descartes.domain.Personne;
+import org.descartes.services.ServicePersonne;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,15 +18,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController 
 @EnableAutoConfiguration 
 public class RestWebService {
+	
+	ServicePersonne servicePersonne = new ServicePersonne();
 
 	@RequestMapping(value = "/personne", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
 	@ResponseBody
-	Personne getPersonne(){
-		return new Personne("Tintin");
+	public List<?> getPersonne(){
+		return servicePersonne.findAll();
 	}
 	
-	void postPersonne(Personne personne){
-		
+	@RequestMapping(value = "/personne/{nom}", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	@ResponseBody
+	public Personne getPersonne(@PathVariable("nom") String nom){
+		return servicePersonne.findPersonne(nom);
+	}
+	
+	public void postPersonne(Personne personne){
+		servicePersonne.addPersonne(personne);
 	}
 }
